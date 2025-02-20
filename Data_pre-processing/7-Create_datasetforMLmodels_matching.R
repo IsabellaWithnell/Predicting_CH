bf23cff5eed3f2e68fbe8f0053f1567b9086e985
Isabella_Withnell <121874515+IsabellaWithnell@users.noreply.github.com>
2025-02-20 18:46:21 +0000

    Update 7-Create_datasetforMLmodels_matching.R

--- Data_pre-processing/7-Create_datasetforMLmodels_matching.R
+++ Data_pre-processing/7-Create_datasetforMLmodels_matching.R
@@ -1,125 +1,125 @@
+# =============================================================================
+# 1. LOAD LIBRARIES & SET WORKING DIRECTORY
+# =============================================================================
 library(Matching)
-library(dplyr)
-
-setwd("~/Summerproject/Descfriptive_analysis")
-impu <- load(file='imputation.Rda')
-impucov <- load(file='imputationcovariates.Rda')
-noimpu <- readRDS('noimputationnew.rds')
-
-data <- imputednew[[1]]
-data <- as.data.frame(data)
-covdata <- imputedcov[[1]]
-covdata <- as.data.frame(covdata)
-
-datap <- merge(covdata, data, by.x="eid", by.y="eid")
-datap <- merge(datap, noimpu, by.x="eid", by.y="eid")
-
-
-datap<-subset(datap, clone!="small")
-datap$clone <- droplevels(datap$clone)
-
-
-levels(datap$symbol)
-
-#Nchange the symbol here to the gene you want
-datap<-subset(datap, symbol=="No_mutation" | symbol=="JAK2")
-
-datap$symbol <- droplevels(datap$symbol)
-
-datap$symbol <- factor(datap$symbol, 
-                       levels=c("JAK2", "No_mutation"), 
-                       labels=c("TRUE", "FALSE"))
-
-
-datap$symbol=ifelse(datap$symbol=="TRUE",1,0)
-
-levels(datap$symbol) <- c(TRUE,FALSE)
-
-datap$symbol <- as.logical(datap$symbol)
-
-table(datap$symbol )
-Tr <- datap$symbol
-eid <- datap$eid
-
-covariates <- datap %>% dplyr::select(sex, age, batch)
-
-X <- data.matrix(covariates)
-rr <- Match(Y=eid, Tr=Tr, X=X, M=1, ties=FALSE)
-
-new_list <- rr[[6]] 
-str(new_list)
-YDF <- new_list[[1]] 
-str(YDF)
-
-
-EID <- new_list[[1]] 
-EID <- as.data.frame(EID) 
-
-NUM <- new_list[[3]] 
-str(NUM)
-typeof(NUM)
-
-nnum <- as.data.frame(NUM) 
-
-together <- cbind(nnum, EID)
-
-saveRDS(together, file="together.rds")
-
-blended <- together
-
-# Merge with datap 
 library(dplyr)
 library(MASS)
 
-Merged <- merge(blended, datap, by.x="EID", by.y="eid")
-
-saveRDS(Merged, file="geneJAK2OCT.rds")
-
-#Cleaning
-
 setwd("~/Summerproject/Descfriptive_analysis")
 
-merge <- readRDS("geneJAK2OCT.rds")
+# =============================================================================
+# 2. LOAD DATASETS
+# =============================================================================
+# Load imputation objects 
+impu    <- load(file = "imputation.Rda")           
+impucov <- load(file = "imputationcovariates.Rda")  
+noimpu  <- readRDS("noimputationnew.rds")
 
-Mergednew <- merge %>% dplyr::select(-EID, -Had_menopause, -sex.x, -age.x, -batch.x)
+data    <- as.data.frame(imputednew[[1]])
+covdata <- as.data.frame(imputedcov[[1]])
 
-colnames(Mergednew) = gsub(pattern = "y*", replacement = "", x = colnames(Mergednew))
+datap <- merge(covdata, data, by = "eid")
+datap <- merge(datap, noimpu, by = "eid")
 
-colnames(Mergednew) = gsub(pattern = "\\.*", replacement = "", x = colnames(Mergednew))
+# =============================================================================
+# 3. SUBSET DATA: CLONE AND SYMBOL SELECTION
+# =============================================================================
+# Exclude records with clone equal to "small"
+datap <- subset(datap, clone != "small")
+datap$clone <- droplevels(datap$clone)
+
+# Subset to include only records with symbol "No_mutation" or "JAK2"
+datap <- subset(datap, symbol %in% c("No_mutation", "JAK2"))
+datap$symbol <- droplevels(datap$symbol)
+
+# Recode symbol so that "JAK2" becomes "TRUE" and "No_mutation" becomes "FALSE"
+datap$symbol <- factor(datap$symbol, 
+                       levels = c("JAK2", "No_mutation"), 
+                       labels = c("TRUE", "FALSE"))
+
+# Convert to numeric (1 for TRUE, 0 for FALSE) and then to logical
+datap$symbol <- ifelse(datap$symbol == "TRUE", 1, 0)
+levels(datap$symbol) <- c(TRUE, FALSE)
+datap$symbol <- as.logical(datap$symbol)
+
+
+# =============================================================================
+# 4. MATCHING PROCEDURE
+# =============================================================================
+Tr <- datap$symbol
+eid <- datap$eid
+
+# Select matching covariates (sex, age, batch) and create a matching matrix
+covariates <- datap %>% select(sex, age, batch)
+X <- data.matrix(covariates)
+
+# Perform 1:1 matching using the Matching package
+rr <- Match(Y = eid, Tr = Tr, X = X, M = 1, ties = FALSE)
+
+# Extract the matching results from rr[[6]]
+new_list <- rr[[6]]
+
+# Extract EID and matching numbers, then combine into one data frame
+EID <- as.data.frame(new_list[[1]])
+NUM <- as.data.frame(new_list[[3]])
+together <- cbind(NUM, EID)
+
+saveRDS(together, file = "together.rds")
+
+# =============================================================================
+# 5. MERGE MATCHING RESULTS WITH ORIGINAL DATA
+# =============================================================================
+blended <- together
+Merged <- merge(blended, datap, by.x = "EID", by.y = "eid")
+saveRDS(Merged, file = "geneJAK2OCT.rds")
+
+# =============================================================================
+# 6. CLEANING & FINAL DATA PREPARATION
+# =============================================================================
+setwd("~/Summerproject/Descfriptive_analysis")
+merged_data <- readRDS("geneJAK2OCT.rds")
+
+# Remove unwanted columns: EID, Had_menopause, sex.x, age.x, batch.x
+Mergednew <- merged_data %>% 
+  select(-EID, -Had_menopause, -sex.x, -age.x, -batch.x)
+
+# Clean column names
+colnames(Mergednew) <- gsub(pattern = "y*", replacement = "", x = colnames(Mergednew))
+colnames(Mergednew) <- gsub(pattern = "\\.*", replacement = "", x = colnames(Mergednew))
 
 datasetformodels <- Mergednew
 
 setwd("~/Summerproject/outputs")
 
+# Rename columns 
+datasetformodels <- datasetformodels %>% 
+  rename(
+    Symbol = smbol,
+    Sex    = sex,
+    Age    = age,
+    Batch  = batch,
+    Pack_years_smoking = Pack_ears_smoking,
+    Alcohol_intake_frequency = Alcohol_intake_frequenc
+  )
 
-datasetformodels <- rename(datasetformodels, Symbol = smbol)
-datasetformodels <- rename(datasetformodels, Sex = sex)
-datasetformodels <- rename(datasetformodels, Age = age)
-datasetformodels <- rename(datasetformodels, Batch = batch)
-datasetformodels <- rename(datasetformodels, Pack_years_smoking = Pack_ears_smoking)
-datasetformodels <- rename(datasetformodels, Alcohol_intake_frequency = Alcohol_intake_frequenc)
+datasetformodels <- datasetformodels %>% select(-CH, -clone)
 
-datasetformodels <- datasetformodels %>% dplyr::select( -CH, -clone)
+datasetformodels$Alcohol_intake_frequency <- as.factor(datasetformodels$Alcohol_intake_frequency)
 
-datasetformodels$Alcohol_intake_frequency<- as.factor(datasetformodels$Alcohol_intake_frequency)
+datasetformodels$Symbol <- factor(datasetformodels$Symbol, labels = c("No", "Yes"))
 
-datasetformodels$Symbol<- factor(datasetformodels$Symbol, labels=c("No", "Yes"))
+datasetformodels$Batch <- as.numeric(datasetformodels$Batch)
+datasetformodels$Age   <- as.numeric(datasetformodels$Age)
 
-datasetformodels$Batch <- as.numeric(datasetformodels$Batch) 
+# =============================================================================
+# 7. COMBINE DATA FOR MODELING & SAVE FINAL DATASET
+# =============================================================================
+xdata <- datasetformodels %>% select(-BMI, -Symbol)
+xall  <- datasetformodels %>% select(BMI, Symbol)
+data_final <- cbind(xdata, xall)
 
-datasetformodels$Age <- as.numeric(datasetformodels$Age) 
+saveRDS(data_final, file = "geneJAK2OCT.rds")
 
-xdata = datasetformodels %>%  dplyr::select(-BMI, -Symbol)
-xall = datasetformodels %>% dplyr::select(BMI, Symbol)
-data= cbind(xdata, xall)
-
-saveRDS(data, file = "geneJAK2OCT.rds")
-
-sum(is.na(data))
-write.csv(data,"~/Summerproject/outputs/python_geneJAK2oct.csv", row.names = FALSE)
-
-
-
-
-
+print(sum(is.na(data_final)))
+write.csv(data_final, "~/Summerproject/outputs/python_geneJAK2oct.csv", row.names = FALSE)
 
